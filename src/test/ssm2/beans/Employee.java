f43edde276d491239c332a22f8a51d1e563e7de2
gitAccPM <wanxuwegen@163.com>
2018-05-17 11:50:02 +0800

    modifed main function

--- src/test/ssm2/beans/Employee.java
+++ src/test/ssm2/beans/Employee.java
@@ -9,9 +9,6 @@ public class Employee {
 	private Department department;
 	public Employee() {
 	}
-	public static void main(String[] args){
-		System.out.println("123");
-	}
 	public Employee(int id, String name, String gender, String email, String departmentId) {
 		this.id = id;
 		this.name = name;
